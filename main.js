ad2958dc03e14bf88754d451c3313e93d6a4c625
Janes Roberta <108099951+janesroberta@users.noreply.github.com>
2022-11-02 16:31:39 -0300

    init-javascript

---
+++ main.js
@@ -0,0 +1,30 @@
+function createGame(player1, hour, player2) {
+  return`
+  <li>
+          <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+          <strong>${hour}</strong>
+          <img src="./assets/icon-${player2}.svg" alt="Bandeira do ${player2}">
+  </li>
+ `
+}
+function createCard(date, day, games) {
+  return `
+    <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW">
+  </header>
+  <main id="cards">
+    ${createCard("24/11","quinta",createGame("brazil", "16:00", "serbia"))}
+    ${createCard("28/11", "segunda",createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay") 
+    )}
+    ${createCard("02/12", "sexta",createGame("brazil", "16:00", "cameroon"))}  
+  </main>
+`
